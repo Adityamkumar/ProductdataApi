89df8ca58082a8a520daa456022d7a8ffcffee40
Aditya kumar <adityasharma841460@gmail.com>
2024-08-24 23:40:03 +0530

    change

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require("express")
 const cors=require("cors")
+const fs=require("fs")
 const artData=require("./artData.json")
 const app=express()
 const PORT=8000;
-
+app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
 app.get("/api/artData",(req,res)=>{
     return res.json(artData);
+})
+app.post("/api/artData",(req,res)=>{
+    const body=req.body;
+    artData.push({...body,id:artData.length+1});
+    fs.writeFile('./artData.json',JSON.stringify(artData),(err,data)=>{
+        return res.json({status:"success",id:artData.length})
+    })
 })
 app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
