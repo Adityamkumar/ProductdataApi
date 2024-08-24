aeb913ef3e372887fde880d19c92530800622738
Aditya kumar <adityasharma841460@gmail.com>
2024-08-24 19:56:31 +0530

    Product Data

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const cors=require("cors")
+const artData=require("./artData.json")
+const app=express()
+const PORT=8000;
+
+app.use(cors())
+
+app.get("//artData",(req,res)=>{
+    return res.json(artData);
+})
+app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
